37333cda3820ddfa2900ea481df4303928ad4e9d
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2022-03-26 22:48:21 +0200

    Tweaks on comments functionality

--- js/comments.js
+++ js/comments.js
@@ -1,13 +1,26 @@
 const btn = document.querySelector("#btn-input");
-document.getElementsByClassName("card")[0];
+const comments = document.querySelector(".comments");
+
+function postComment(name, message) {
+  const div = `
+  <div class="commentsBox">
+            <h2>Comment By <span>${name.value}</span> </h2>
+            <p>${message.value}</p>
+        </div>
+  `;
+  comments.innerHTML += div;
+  return 1;
+}
 
 btn.addEventListener("click", (e) => {
   e.preventDefault();
   const inputs = [
     document.querySelector("#name-input"),
-    document.querySelector("#email-input"),
     document.querySelector("#message-input"),
+    document.querySelector("#email-input"),
   ];
-
-  console.log(inputs[0].value);
+  postComment(inputs[0], inputs[1]);
+  inputs.forEach((e) => {
+    e.value = "";
+  });
 });
