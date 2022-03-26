dcdf4e7fe97e7c95a0868adca72051a98b58d4a9
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2022-03-26 11:57:48 +0200

    Add firebase connection

---
+++ js/db connection.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBbHG5IuvmfVHk60Zft3fIjgUxc13ghzDg",
+  authDomain: "kids-center-diploma-project.firebaseapp.com",
+  projectId: "kids-center-diploma-project",
+  storageBucket: "kids-center-diploma-project.appspot.com",
+  messagingSenderId: "805370464526",
+  appId: "1:805370464526:web:35d3191115fb77193a6f5f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
